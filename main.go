9a1d935790d94a9ce8d3f7ed0d0526b629799fd4
Özgür Yalçın <yalcin.ozqur@gmail.com>
2020-08-14 03:15:54 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	turkpos "github.com/OzqurYalcin/turkpos/src"
+	uuid "github.com/google/uuid"
 )
 
 func main() {
-	api := &turkpos.API{"T"} // "T","P"
+	api := &turkpos.API{"T"} // "T": test, "P": production
 	request := &turkpos.Request{}
 	request.Body.Payment.G.ClientCode = "10738"    // Müşteri No
 	request.Body.Payment.G.ClientUsername = "Test" // Kullanıcı adı
 	request.Body.Payment.G.ClientPassword = "Test" // Şifre
 	// Ödeme
+	commission := 0.0094
+	amount := 100.00
+	request.Body.Payment.GUID = "0c13d406-873b-403b-9c09-a5766840d98c"
+	request.Body.Payment.Security = "NS"
+	request.Body.Payment.OrderID = uuid.New().String()
+	request.Body.Payment.PosID = 1029 // yurtdışı 1023
+	request.Body.Payment.Description = "Açıklama"
+	request.Body.Payment.CardOwner = "Kart sahibi"
+	request.Body.Payment.CardNumber = "4546711234567894"
+	request.Body.Payment.CardMonth = "12"
+	request.Body.Payment.CardYear = "2026"
+	request.Body.Payment.CardCvc = "000"
+	request.Body.Payment.GsmNumber = "5554443322"
+	request.Body.Payment.Price = strings.ReplaceAll(fmt.Sprintf("%.2f", amount-(amount*commission)), ".", ",")
+	request.Body.Payment.Amount = strings.ReplaceAll(fmt.Sprintf("%.2f", amount), ".", ",")
+	request.Body.Payment.Installment = 1
+	request.Body.Payment.IPAddr = "85.34.78.112"
+	request.Body.Payment.Referer = "https://www.example.com/payment"
+	request.Body.Payment.CallbackError = "https://www.example.com/payment"
+	request.Body.Payment.CallbackSuccess = "https://www.example.com/payment"
+	request.Body.Payment.Hash = ""
 
 	response := api.Payment(request)
-	if response.Body.Response.Result.TransactionID != "" {
-
-	}
+	fmt.Println(response.Body.Payment.Result.Message)
 }
--- src/turkpos.go
+++ src/turkpos.go
@@ -20,18 +20,20 @@ type API struct {
 
 type Request struct {
 	XMLName xml.Name `xml:"soap:Envelope,omitempty"`
+	Soap    string   `xml:"xmlns:soap,attr"`
 	Body    struct {
 		XMLName xml.Name `xml:"soap:Body,omitempty"`
 		Payment struct {
-			G struct {
+			NS string `xml:"xmlns,attr"`
+			G  struct {
 				ClientCode     interface{} `xml:"CLIENT_CODE,omitempty"`
 				ClientUsername interface{} `xml:"CLIENT_USERNAME,omitempty"`
 				ClientPassword interface{} `xml:"CLIENT_PASSWORD,omitempty"`
 			} `xml:"G,omitempty"`
 			GUID            interface{} `xml:"GUID,omitempty"`
-			POSID           interface{} `xml:"SanalPOS_ID,omitempty"`
-			HASH            interface{} `xml:"Islem_Hash,omitempty"`
+			Hash            interface{} `xml:"Islem_Hash,omitempty"`
 			Security        interface{} `xml:"Islem_Guvenlik_Tip,omitempty"`
+			PosID           interface{} `xml:"SanalPOS_ID,omitempty"`
 			OrderID         interface{} `xml:"Siparis_ID,omitempty"`
 			Description     interface{} `xml:"Siparis_Aciklama,omitempty"`
 			CardOwner       interface{} `xml:"KK_Sahibi,omitempty"`
@@ -53,25 +55,32 @@ type Request struct {
 			Data4           interface{} `xml:"Data4,omitempty"`
 			Data5           interface{} `xml:"Data5,omitempty"`
 		} `xml:"TP_Islem_Odeme,omitempty"`
-	} `xml:"soap:Body,omitempty"`
+	}
 }
 
 type Response struct {
 	XMLName xml.Name `xml:"soap:Envelope,omitempty"`
+	Soap    string   `xml:"xmlns:soap,attr"`
+	Xsi     string   `xml:"xmlns:xsi,attr"`
+	Xsd     string   `xml:"xmlns:xsd,attr"`
 	Body    struct {
-		Response struct {
+		XMLName xml.Name `xml:"soap:Body,omitempty"`
+		Payment struct {
+			NS     string `xml:"xmlns,attr"`
 			Result struct {
-				Result        string `xml:"Banka_Sonuc_Kod,omitempty"`
-				Code          string `xml:"Sonuc,omitempty"`
-				Message       string `xml:"Sonuc_Str,omitempty"`
-				URL           string `xml:"UCD_URL,omitempty"`
-				TransactionID string `xml:"Islem_ID,omitempty"`
+				TransactionID interface{} `xml:"Islem_ID,omitempty"`
+				URL           interface{} `xml:"UCD_URL,omitempty"`
+				Code          interface{} `xml:"Sonuc,omitempty"`
+				Message       interface{} `xml:"Sonuc_Str,omitempty"`
+				Result        interface{} `xml:"Banka_Sonuc_Kod,omitempty"`
 			} `xml:"TP_Islem_OdemeResult,omitempty"`
 		} `xml:"TP_Islem_OdemeResponse,omitempty"`
-	} `xml:"soap:Body,omitempty"`
+	}
 }
 
 func (api *API) Payment(request *Request) (response *Response) {
+	request.Soap = "http://schemas.xmlsoap.org/soap/envelope/"
+	request.Body.Payment.NS = "https://turkpos.com.tr/"
 	response = new(Response)
 	postdata, _ := xml.Marshal(request)
 	res, err := http.Post(Modes[api.Mode]+"?op=TP_Islem_Odeme", "text/xml; charset=utf-8", strings.NewReader(strings.ToLower(xml.Header)+string(postdata)))
